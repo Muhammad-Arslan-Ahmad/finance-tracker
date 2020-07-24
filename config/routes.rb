ec394cb0e1fe281ccce66fa42f174251505bf5e4
arslan <arslan.ahmad@devbox.co>
2020-07-24 14:25:40 +0500

    till second last

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'
   root 'welcome#index'
+  resources :friendships, only: [:create, :destroy]
 end
